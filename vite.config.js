0f82314cdb4b65205d91d7146b10782b748ec599
Rana16468 <amsohelrana.me@gmail.com>
2025-03-04 09:00:07 +0600

    vercel issues resolve

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "agora-vendor": ["agora-react-uikit", "agora-rtm-sdk"],
+          "daisyui-components": ["daisyui"],
+          // Add more chunks as needed
+        },
+      },
+    },
+    // Optionally increase the warning limit if needed
+    chunkSizeWarningLimit: 800,
+  },
+});
